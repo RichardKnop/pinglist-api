bf94e1d5ab222e9486fa136a36c21ee7586efda6
Richard Knop <risoknop@gmail.com>
2016-03-26 19:39:14 +0800

    Added more test for getting max alarms limit.

--- alarms/max_alarm_limits_test.go
+++ alarms/max_alarm_limits_test.go
@@ -6,59 +6,158 @@ import (
 	"github.com/RichardKnop/pinglist-api/accounts"
 	"github.com/RichardKnop/pinglist-api/subscriptions"
 	"github.com/RichardKnop/pinglist-api/teams"
+	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 )
 
-func (suite *AlarmsTestSuite) TestGetMaxAlarms() {
-	var (
-		user      *accounts.User
-		maxAlarms int
-	)
-
-	user = new(accounts.User)
+func (suite *AlarmsTestSuite) TestGetMaxAlarmsNoSubscriptionUserInTrialPeriod() {
+	user := new(accounts.User)
 	*user = *suite.users[1]
 
-	// Mock find team
+	// User is not a member of a team
 	suite.mockFindTeamByMemberID(
 		suite.users[1].ID,
 		nil,
 		teams.ErrTeamNotFound,
 	)
 
-	// Mock find active subscription
+	// User does not have a personal subscription
 	suite.mockFindActiveSubscriptionByUserID(
 		suite.users[1].ID,
 		nil,
 		subscriptions.ErrUserHasNoActiveSubscription,
 	)
 
-	// Without a team and without a subscription, but the user is in free trial
+	// User is in a free trial period
 	user.CreatedAt = time.Now()
-	maxAlarms = suite.service.getMaxAlarms(user)
+
+	// Max alarms should default to the free trial constant
+	maxAlarms := suite.service.getMaxAlarms(user)
 	assert.Equal(suite.T(), subscriptions.FreeTrialMaxAlarms, maxAlarms)
 
 	// Check that the mock object expectations were met
 	suite.assertMockExpectations()
+}
 
-	// Mock find team
+func (suite *AlarmsTestSuite) TestGetMaxAlarmsNoSubscriptionUserNotInTrialPeriod() {
+	user := new(accounts.User)
+	*user = *suite.users[1]
+
+	// User is not a member of a team
 	suite.mockFindTeamByMemberID(
 		suite.users[1].ID,
 		nil,
 		teams.ErrTeamNotFound,
 	)
 
-	// Mock find active subscription
+	// User does not have a personal subscription
 	suite.mockFindActiveSubscriptionByUserID(
 		suite.users[1].ID,
 		nil,
 		subscriptions.ErrUserHasNoActiveSubscription,
 	)
 
-	// Without a team and without a subscription, and no longer in free trial
+	// User is no longer in a free trial period
 	user.CreatedAt = time.Now().Add(-31 * 24 * time.Hour)
-	maxAlarms = suite.service.getMaxAlarms(user)
+
+	// Max alarms should be zero
+	maxAlarms := suite.service.getMaxAlarms(user)
 	assert.Equal(suite.T(), 0, maxAlarms)
 
 	// Check that the mock object expectations were met
 	suite.assertMockExpectations()
+}
+
+func (suite *AlarmsTestSuite) TestGetMaxAlarmsTeamWithSubscriptionUserWithoutSubscription() {
+	user := new(accounts.User)
+	*user = *suite.users[1]
+
+	// User is a member of a team
+	suite.mockFindTeamByMemberID(
+		suite.users[1].ID,
+		&teams.Team{Owner: &accounts.User{Model: gorm.Model{ID: 123}}},
+		nil,
+	)
+
+	// The team has an active subscription
+	suite.mockFindActiveSubscriptionByUserID(
+		123,
+		&subscriptions.Subscription{
+			Plan: &subscriptions.Plan{
+				MaxAlarms: 100,
+			},
+		},
+		nil,
+	)
+
+	// Max alarms should be taken from the team subscription
+	maxAlarms := suite.service.getMaxAlarms(user)
+	assert.Equal(suite.T(), 100, maxAlarms)
+
+	// Check that the mock object expectations were met
+	suite.assertMockExpectations()
+}
+
+func (suite *AlarmsTestSuite) TestGetMaxAlarmsTeamWithoutSubscriptionUserWithSubscription() {
+	user := new(accounts.User)
+	*user = *suite.users[1]
+
+	// User is a member of a team
+	suite.mockFindTeamByMemberID(
+		suite.users[1].ID,
+		&teams.Team{Owner: &accounts.User{Model: gorm.Model{ID: 123}}},
+		nil,
+	)
+
+	// The team has no active subscription
+	suite.mockFindActiveSubscriptionByUserID(
+		123,
+		nil,
+		subscriptions.ErrUserHasNoActiveSubscription,
+	)
+
+	// User has an active subscription
+	suite.mockFindActiveSubscriptionByUserID(
+		suite.users[1].ID,
+		&subscriptions.Subscription{
+			Plan: &subscriptions.Plan{
+				MaxAlarms: 10,
+			},
+		},
+		nil,
+	)
+
+	// Max alarms should be taken from the user subscription
+	maxAlarms := suite.service.getMaxAlarms(user)
+	assert.Equal(suite.T(), 10, maxAlarms)
+
+	// Check that the mock object expectations were met
+	suite.assertMockExpectations()
+}
+
+func (suite *AlarmsTestSuite) TestGetMaxAlarmsUserWithSubscription() {
+	user := new(accounts.User)
+	*user = *suite.users[1]
+
+	// User is not a member of a team
+	suite.mockFindTeamByMemberID(
+		suite.users[1].ID,
+		nil,
+		teams.ErrTeamNotFound,
+	)
+
+	// User has an active subscription
+	suite.mockFindActiveSubscriptionByUserID(
+		suite.users[1].ID,
+		&subscriptions.Subscription{
+			Plan: &subscriptions.Plan{
+				MaxAlarms: 10,
+			},
+		},
+		nil,
+	)
+
+	// Max alarms should be taken from the user subscription
+	maxAlarms := suite.service.getMaxAlarms(user)
+	assert.Equal(suite.T(), 10, maxAlarms)
 }
