96d014371bdbed88d9eee3e1597fa341ef419b48
Richard Knop <risoknop@gmail.com>
2016-04-12 16:24:26 +0100

    Fixed broken interface.

--- subscriptions/service_interface.go
+++ subscriptions/service_interface.go
@@ -19,7 +19,6 @@ type ServiceInterface interface {
 	FindCardByCardID(cardID string) (*Card, error)
 	FindSubscriptionByID(subscriptionID uint) (*Subscription, error)
 	FindSubscriptionBySubscriptionID(subscriptionID string) (*Subscription, error)
-	FindSubscriptionByCardID(cardID uint) (*Subscription, error)
 	FindActiveSubscriptionByUserID(userID uint) (*Subscription, error)
 
 	// Needed for the newRoutes to be able to register handlers
